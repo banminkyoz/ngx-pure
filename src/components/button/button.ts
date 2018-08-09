daf4276cc2a669ec2b085b762a787c0c365311e2
Kyoz <banminkyoz@gmail.com>
2018-08-09 23:53:31 +0700

    Update button

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -3,13 +3,15 @@ import { PureComponent } from '../../class/component';
 
 const BUTTON_HOST_ATTRIBUTES = [
   'pure-button',
+  'pure-flat-button',
   'pure-raised-button',
+  'pure-stroked-button',
   'pure-button-round'
 ];
 
 @Component({
-  selector: `button[pure-button], button[pure-raised-button], button[pure-round-button],
-             a[pure-button], a[pure-raised-button], a[pure-round-button]`,
+  selector: `button[pure-button], button[pure-flat-button], button[pure-raised-button], button[pure-stroked-button], button[pure-round-button],
+             a[pure-button], a[pure-flat-button], a[pure-raised-button], a[pure-stroked-button], a[pure-round-button]`,
   exportAs: 'pureButton',
   templateUrl: './button.html',
   styleUrls: ['./button.scss'],
