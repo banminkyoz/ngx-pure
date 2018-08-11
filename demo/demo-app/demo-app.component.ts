e5670ad6128a75d7ee09e2515be8d058add9d096
Kyoz <banminkyoz@gmail.com>
2018-08-11 23:06:12 +0700

    Add LTR & Fullscreen support in demo

--- demo/demo-app/demo-app.component.ts
+++ demo/demo-app/demo-app.component.ts
@@ -6,10 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo-app.component.scss']
 })
 export class DemoApp {
-  title = 'app';
+  dirState: 'LTR' | 'RTL' = 'LTR';
   menuItems = [
-    {name: 'Button', route: '/button'},
-    {name: 'Container', route: '/container'},
-    {name: 'Text', route: '/text'}
+    { name: 'Button', route: '/button' },
+    { name: 'Container', route: '/container' },
+    { name: 'Text', route: '/text' }
   ];
+
+  changeDirState() {
+    this.dirState = this.dirState === 'LTR' ? 'RTL' : 'LTR';
+    document.getElementById('demo').setAttribute('dir', this.dirState);
+  }
+
+  requestFullscreen() {
+    const demo: any = document.getElementById('demo');
+    if (demo.requestFullscreen) {
+      demo.requestFullscreen();
+    } else if (demo.mozRequestFullScreen) { /* Firefox */
+      demo.mozRequestFullScreen();
+    } else if (demo.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      demo.webkitRequestFullscreen();
+    } else if (demo.msRequestFullscreen) { /* IE/Edge */
+      demo.msRequestFullscreen();
+    }
+  }
 }
